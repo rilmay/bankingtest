a8cd663a57f93093af46bf5b07eebecd3b12c9db
reeelmay <nikita.1999.slipknot@gmail.com>
2022-01-07 22:25:09 +0300

    working hibernate entity

--- src/main/java/com/guzov/controller/GreetingController.java
+++ src/main/java/com/guzov/controller/GreetingController.java
@@ -1,14 +1,22 @@
 package com.guzov.controller;
 
+import com.guzov.domain.Message;
+import com.guzov.repostory.MessageRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.view.RedirectView;
 
+import java.util.List;
 import java.util.Map;
 
 @Controller
 public class GreetingController {
+    @Autowired
+    private MessageRepo messageRepo;
 
     @GetMapping("/greeting")
     public String greeting(
@@ -21,7 +29,31 @@ public class GreetingController {
 
     @GetMapping("/")
     public String main(Map<String, Object> model) {
-        model.put("some", "nikita");
+        Iterable<Message> messages = messageRepo.findAll();
+
+        model.put("messages", messages);
+        return "main";
+    }
+
+    @PostMapping(value = "/")
+    public RedirectView add(
+            @RequestParam String text,
+            @RequestParam String tag
+    ) {
+        Message message = new Message(text, tag);
+        messageRepo.save(message);
+        return new RedirectView("");
+    }
+
+    @PostMapping("filter")
+    public String filter(@RequestParam String tag, Map<String, Object> model) {
+        Iterable<Message> messages;
+        if (tag == null || tag.isEmpty()) {
+            messages = messageRepo.findAll();
+        } else {
+            messages= messageRepo.findByTag(tag);
+        }
+        model.put("messages", messages);
         return "main";
     }
 
---
+++ src/main/java/com/guzov/repostory/MessageRepo.java
@@ -0,0 +1,10 @@
+package com.guzov.repostory;
+
+import com.guzov.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MessageRepo extends CrudRepository<Message, Long> {
+    List<Message> findByTag(String tag);
+}
