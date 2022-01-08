f02730c93295e9ce355e6e5aa3be3f3394e22d1f
reeelmay <nikita.1999.slipknot@gmail.com>
2022-01-08 13:41:25 +0300

    added user service

--- src/main/java/com/guzov/controller/MainController.java
+++ src/main/java/com/guzov/controller/MainController.java
@@ -1,16 +1,16 @@
 package com.guzov.controller;
 
 import com.guzov.domain.Message;
+import com.guzov.domain.User;
 import com.guzov.repostory.MessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.view.RedirectView;
 
-import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -34,10 +34,11 @@ public class MainController {
 
     @PostMapping("/main")
     public RedirectView add(
+            @AuthenticationPrincipal User user,
             @RequestParam String text,
             @RequestParam String tag
     ) {
-        Message message = new Message(text, tag);
+        Message message = new Message(text, tag, user);
         messageRepo.save(message);
         return new RedirectView("/main");
     }
@@ -48,7 +49,7 @@ public class MainController {
         if (tag == null || tag.isEmpty()) {
             messages = messageRepo.findAll();
         } else {
-            messages= messageRepo.findByTag(tag);
+            messages = messageRepo.findByTag(tag);
         }
         model.put("messages", messages);
         return "main";
