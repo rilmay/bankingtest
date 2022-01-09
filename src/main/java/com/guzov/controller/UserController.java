01ddfa5cd17c433d6fa9c9b4a6094fc116fc15af
reeelmay <nikita.1999.slipknot@gmail.com>
2022-01-10 00:44:48 +0300

    added user edit pages

---
+++ src/main/java/com/guzov/controller/UserController.java
@@ -0,0 +1,53 @@
+package com.guzov.controller;
+
+import com.guzov.domain.Role;
+import com.guzov.domain.User;
+import com.guzov.repostory.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Controller
+@RequestMapping("/user")
+@PreAuthorize("hasAnyAuthority('ADMIN')")
+public class UserController {
+    @Autowired
+    private UserRepo userRepo;
+
+    @GetMapping
+    public String userList(Map<String, Object> model) {
+        model.put("users", userRepo.findAll());
+        return "userList";
+    }
+
+    @GetMapping("{user}")
+    public String userEditForm(@PathVariable User user, Map<String, Object> model) {
+        model.put("user", user);
+        model.put("roles", Role.values());
+        return "userEdit";
+    }
+
+    @PostMapping
+    public String userSave(
+            @RequestParam String username,
+            @RequestParam Map<String, String> form,
+            @RequestParam("userId") User user
+    ) {
+        user.setUsername(username);
+
+        Set<Role> roles = form.keySet()
+                .stream()
+                .filter(x -> Arrays.stream(Role.values()).map(Enum::name).anyMatch(role -> role.equals(x)))
+                .map(Role::valueOf)
+                .collect(Collectors.toSet());
+        user.setRoles(roles);
+        userRepo.save(user);
+        return "redirect:/user";
+    }
+}
